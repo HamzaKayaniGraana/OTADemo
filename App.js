136b235acc7ccb3ca9cdb37989e9316126ebcaf0
Hamza Mehmood <hamzamehmood@Hamzas-MacBook-Pro.local>
2022-03-30 13:03:20 +0500

    Changes

--- App.js
+++ App.js
@@ -86,6 +86,7 @@ const App = () => {
           </Section>
           <Section title="Learn More">
             Read the docs to discover what to do next:
+            hamza mehmoood\dshgdlsj
           </Section>
           <LearnMoreLinks />
         </View>
